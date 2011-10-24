c4906ff26c45d962843610463e51d33c8ee3c7ad
Priya Dharshini <priya.t@imaginea.com>
2011-10-24 16:37:54 +0530

    WYSIWYG implemented and scopes added to interview model

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,2 +1,5 @@
 class Interview < ActiveRecord::Base
+  scope :today, where(:scheduled_at => Date.today)
+  scope :this_week, where("scheduled_at <= ?", Date.today.end_of_week)
+  scope :this_month, where("scheduled_at <= ?", Date.today.end_of_month)
 end
