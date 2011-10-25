6e9f44ba3b0d13a6ba5aabc49db526523049d286
Priya Dharshini <priya.t@imaginea.com>
2011-10-25 15:27:53 +0530

    Header alignment and Jbar done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,13 @@
 module ApplicationHelper
-
   def show_candidate_status(c)
     c.status.nil? ? "New" : c.status
   end
-  
+
+  def flash_notifications
+    message = flash[:error] || flash[:notice] || flash[:alert]
+    if message
+    type = flash.keys[0].to_s
+    "<script>$(function() { $(\"#notification_bar\").bar({background_color:'#fbef75',message:\"#{message}\"})\; })\;</script>".html_safe
+    end
+  end
 end
