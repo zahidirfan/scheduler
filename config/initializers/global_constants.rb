5bf27ebf2c9e8c037190c898c347b91303cd2b8a
Gokula Murthy <gokul.m@imaginea.com>
2011-11-15 14:51:49 +0530

    few global methods were replaced with global constants

---
+++ config/initializers/global_constants.rb
@@ -0,0 +1,20 @@
+USER_ROLES = {
+  "Business Manager" => "Bm",
+  "Human Resource" => "Hr",
+  "Interviewer" => "Interviewer",
+  "Project Lead" => "Pl",
+  "Administrator" => "Administrator"
+}
+
+FEEDBACK_STATUS = {
+  "Clear" => "Clear",
+  "Hold" => "Hold",
+  "Drop" => "Drop"
+}
+
+HIRING_STATUS = {
+  "Archive" => "Archive",
+  "Hired" => "Hired",
+  "Bench" => "Bench"
+}
+
