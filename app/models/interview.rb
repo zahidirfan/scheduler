7dd3c2724c04199fad7018cb21587e87f31573fc
Manikandan K <manikandan.k@imaginea.com>
2012-04-30 16:23:13 +0530

    distinct added in uncancelled scope instead of using uniq

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -47,14 +47,14 @@ class Interview < ActiveRecord::Base
     self.candidate.update_attribute("status", "Scheduled")
   end
 
-  scope :uncancelled, joins("LEFT OUTER JOIN comments ON comments.interview_id = interviews.id").where("status is null or status != 'Cancelled'")
-  scope :by_date, lambda { |date| where("scheduled_at like '#{date}%'").uncancelled.order("scheduled_at").uniq}
-  scope :later, where("scheduled_at >= ? and scheduled_at < ?", Date.today.next_week, Date.today.end_of_month+1).uncancelled.order("scheduled_at").uniq
-  scope :upcoming, where("scheduled_at  >= ? ", DateTime.current).uncancelled.order("scheduled_at").uniq
-  scope :this_week, where("scheduled_at > ? and scheduled_at < ?", Date.tomorrow+1, Date.today.end_of_week+1).uncancelled.order("scheduled_at").uniq
-  scope :fetch_interviews, lambda { |start, endtime| where("scheduled_at between ? and ? ", Time.at(start.to_i).to_formatted_s(:db), Time.zone.at(endtime.to_i).to_formatted_s(:db)).uncancelled.uniq }
+  scope :uncancelled, joins(:comments).where("status is null or status != 'Cancelled'").select('distinct interviews.id, interviews.*')
+  scope :by_date, lambda { |date| where("scheduled_at like '#{date}%'").uncancelled.order("scheduled_at")}
+  scope :later, where("scheduled_at >= ? and scheduled_at < ?", Date.today.next_week, Date.today.end_of_month+1).uncancelled.order("scheduled_at")
+  scope :upcoming, where("scheduled_at  >= ? ", DateTime.current).uncancelled.order("scheduled_at")
+  scope :this_week, where("scheduled_at > ? and scheduled_at < ?", Date.tomorrow+1, Date.today.end_of_week+1).uncancelled.order("scheduled_at")
+  scope :fetch_interviews, lambda { |start, endtime| where("scheduled_at between ? and ? ", Time.at(start.to_i).to_formatted_s(:db), Time.zone.at(endtime.to_i).to_formatted_s(:db)).uncancelled }
   scope :dummy, where("1 = 1")
-  scope :by_user_id, lambda { |user_id| where("interviews.user_id = ?", user_id).uncancelled.uniq }
+  scope :by_user_id, lambda { |user_id| where("interviews.user_id = ?", user_id).uncancelled }
 
   # this will return the previous record of invoking interview object.
   def previous(offset = 0)
